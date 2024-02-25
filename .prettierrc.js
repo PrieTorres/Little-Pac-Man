c1d82d1e03d8c5b713ad02eb096e33f90dde5569
Priscila <priscila.paula@clinicorp.com>
2024-02-25 20:48:42 -0300

    editor configs

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  semi: true,
+  trailingComa: "all",
+  doubleQuote: true,
+  printWidth: 120,
+  tabWidth: 2,
+  endOfLine: "auto",
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Nothing yet, I'll let you guys know when will be have something here
+
+npx tsc --init

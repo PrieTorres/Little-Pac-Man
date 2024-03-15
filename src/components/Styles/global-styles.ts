aeb54ce0707c3a2bcc13ead4b318c27b8881e2e8
Priscila <priscilatorres2023@gmail.com>
2024-03-14 21:38:16 -0300

    feat(initials): setting main structure

---
+++ src/components/Styles/global-styles.ts
@@ -0,0 +1,29 @@
+import { createGlobalStyle, css } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap');
+
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html{
+    font-size: 62.5%;
+  }
+
+  body{
+    ${({ theme }) => css`
+      font-size: ${theme.font.size.medium};
+      font-family: 'Courier New', Courier, monospace;
+      background: #888;
+      background: ${theme.colors.mainBg};
+      font-family: ${({ theme }) => theme.font.family.default}
+    `}
+  }
+
+  h1, h2, h3, h4, h5, h6{
+    font-family: ${({ theme }) => theme.font.family.secondary}
+  }
+`;
---
+++ src/pages/GameScene.jsx
@@ -0,0 +1,8 @@
+export const GameScene = () => {
+
+    return (
+        <div>
+            
+        </div>
+    );
+}

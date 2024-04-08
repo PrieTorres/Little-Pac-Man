0124736ea3feb5aee335a88376026695862bca4d
Priscila <priscila.paula@clinicorp.com>
2024-04-08 13:41:42 -0300

    Update structural-typing.ts

--- aulas/structural-typing.ts
+++ aulas/structural-typing.ts
@@ -6,7 +6,7 @@ type User = {
 type verifyUserFn = (user: User, sentValue: User) => boolean;
 
 const verifyUser: verifyUserFn = (user, sentValue) => {
-  return(
+  return (
     user.userName === sentValue.userName &&
     user.passaword === sentValue.passaword
   )
@@ -18,9 +18,13 @@ const user1 = {
 };
 const user1sv = {
   userName: "user1",
-  passaword: "123"
+  passaword: "123",
+  perms: [
+    { a1: "blablavla" }
+  ]
 };
 
-const logged = verifyUser(user1, user1sv);
+const logged = verifyUser(user1, user1sv);//ele confere os tipos dos valores passados mas user1 e user1sv não possuem tipo, o typescript confere as restrições
+//n tem problema os parametros possuirem chaves a mais, é conferido o necessário e o tipo do necessário
 console.log(logged)
 
